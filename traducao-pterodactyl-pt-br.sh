503e7f4385dbf02ef713da6d68c524e5bba62f45
zGumeloBr <66852780+zGumeloBr@users.noreply.github.com>
2020-08-04 14:47:02 -0300

    Update traducao-pterodactyl-pt-br.sh

--- traducao-pterodactyl-pt-br.sh
+++ traducao-pterodactyl-pt-br.sh
@@ -6,6 +6,12 @@ rm -rf en
 
 git clone https://github.com/zGumeloBr/traducao-pt-br-pterodactyl-panel.git
 
+cd /var/www/pterodactyl/resources/lang/traducao-pt-br-pterodactyl-panel
+
+rm -rf traducao-pterodactyl-pt-br.sh
+
+cd /var/www/pterodactyl/resources/lang
+
 mv traducao-pt-br-pterodactyl-panel en
 
 echo "Tradução finalizada."
